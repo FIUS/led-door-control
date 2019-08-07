f9971532a40544ac8e513af0de56b299e8fcd5f5
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-08-07 13:04:02 +0200

    Improve exception handling
    Change that if setting the door state for the ceiling fails, it is still set for the dishwasher.
    Also improve logging of exceptions again.

--- doorControll.py
+++ doorControll.py
@@ -4,7 +4,6 @@
 import requests
 import os
 import time
-import sys
 
 doorIsOpen = False
 
@@ -15,29 +14,30 @@ def checkDoor():
             setDoorOpen(True)
         else:
             setDoorOpen(False)
-    except:
-        e = sys.exc_info()[0]
-        print("Error: " + str(e))
+    except Exception as e:
+        print("Error in checkDoor: ", e)
 
 def setDoorOpen(state):
     global doorIsOpen
 
     if not(state == doorIsOpen):
-        try:
-            doorIsOpen = state
-            ceilingIP = os.environ['LED_CEILING']
-            if doorIsOpen:
-                print("Door was opened")
-                requests.post("http://"+ceilingIP+"?doorOpen")
-                requests.post("http://led-dishwasher?doorOpen")
-            else:
-                print("Door was closed")
-                requests.post("http://"+ceilingIP+"?doorClosed")
-                requests.post("http://led-dishwasher?doorClosed")
-        except:
-            e = sys.exc_info()[0]
-            print("Error: " + str(e))
+        doorIsOpen = state
+        ceilingIP = os.environ['LED_CEILING']
+        if doorIsOpen:
+            print("Door was opened")
+            doCheckedPostRequest("http://"+ceilingIP+"?doorOpen")
+            doCheckedPostRequest("http://led-dishwasher?doorOpen")
+        else:
+            print("Door was closed")
+            doCheckedPostRequest("http://"+ceilingIP+"?doorClosed")
+            doCheckedPostRequest("http://led-dishwasher?doorClosed")
 
+def doCheckedPostRequest(url):
+    try:
+        requests.post(url)
+    except Exception as e:
+        print("Error in post: ", e)
+        
 while True:
     checkDoor()
     time.sleep(5)
